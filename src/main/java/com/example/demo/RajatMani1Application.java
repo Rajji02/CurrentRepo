c7eac9e43ec2e86957104c0258f688a767636c22
Rajji02 <rajat.agrawal@letsgettin.com>
2019-06-06 15:43:15 +0530

    before adding to remote repsoitory

---
+++ src/main/java/com/example/demo/RajatMani1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RajatMani1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RajatMani1Application.class, args);
+		//
+	}
+
+}
